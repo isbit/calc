84e82e64a4e80371b89612041c8a122966661c12
IcyBrandDesign <ouing83@hotmail.com>
2024-09-16 22:31:22 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,5 +10,11 @@ def sub(x,y):
 sum = sub(10,5)
 print(sum)
 
+def multi(a, b):
+    return a*b
+
+summulti = multi(6, 5)
+print(summulti)
+# print(multi(6, 2))
 #Todo add multiplication, division 
 # Ser du dette?vfghjkkoo
