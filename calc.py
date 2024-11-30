3be79dbaa4021aa77c81e8bb65ea3ce5ce56f8a1
IcyBrandDesign <ouing83@hotmail.com>
2024-11-30 23:30:01 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
-#Todo quadruple. Handtere at brukere gir bokstaver i stedet for tall som input. Det gir feil nå. 
+'''
+Too do:
+
+Handtere at brukere gir bokstaver i stedet for tall som input. Det gir feil nå. 
+'''
+
 
 class Kalkulator:
 
@@ -7,19 +12,12 @@ class Kalkulator:
 
     def fibonacci(self, n):
         resultat = []
-        if n < 1:
-            print("DUST\n"*100)
-        elif n == 1:
-            resultat.append(0)
-        elif n > 10000:
-            print("Maks nummer er 9999")
-        else:
-            resultat.append(0)
-            resultat.append(1)
-            for i in range(1 ,n):
-                tall = resultat[i] + resultat[i-1]
-                resultat.append(tall)
-            return resultat
+        resultat.append(0)
+        resultat.append(1)
+        for i in range(1 ,n-1):
+            tall = resultat[i] + resultat[i-1]
+            resultat.append(tall)
+        return resultat, resultat[n-1]
 
     def add(self, x,y):
         return x+y
@@ -42,29 +40,54 @@ class Kalkulator:
         else: 
             return y                            
 
-
-
-nummer_1 = int(input("Set inn forste tall: "))
-nummer_2 = int(input("Set inn andre tall: "))
-biggest = findBiggest(nummer_1, nummer_2)
-print(f"{biggest} er størst")
-
+kalkulator_objektet = Kalkulator()
+sum = kalkulator_objektet.add(9,4)
+print(sum)
 
 # This is the menu. 
 avslutt = False
 while(not avslutt):
     print("---=== Meny ===---")
     print("0: Avslutt")
-    print("1: Addere")
+    print("1: Addere, Substrahere, Multiplisere, Dele")
+    print("2: Eksponent  n^x ")
+    print("3: Fibonacchi verdi og rekke")
     menuchoice = int(input("Valg:"))
     match menuchoice:
         case 0:
             avslutt = True
             print("Avslutter")
         case 1:
-            forste_tall = int(input("Sett in det første tallet du vi addere: "))
-            andre_tall = int(input("Sett inn tall to: "))
-            add(forste_tall, andre_tall)
+            forste_tall = int(input("Tall 1: "))
+            andre_tall = int(input("Tall 2: "))
+            valg = input("add, sub, multi, dele: ")
+            operasjoner =  {"add": kalkulator_objektet.add(forste_tall, andre_tall),
+            "sub": kalkulator_objektet.sub(forste_tall, andre_tall),
+            "multi" : kalkulator_objektet.multi(forste_tall, andre_tall),
+            "dele" : kalkulator_objektet.dele(forste_tall, andre_tall)}
+            if valg in operasjoner:
+                operasjon = operasjoner[valg]
+                print(f"\nSvar:\n{operasjon}\n")
+            else:
+                print("Ikke riktig input")
+        case 2:
+            forste_tall = int(input("Tall 1: "))
+            andre_tall = int(input("Tall 2: "))
+            resultat = kalkulator_objektet.nterot(forste_tall, andre_tall)
+            print(f"\nSvar:\n{resultat}\n")
+        case 3:
+            antall = int(input("Sett inn antall du ønsker: "))
+            resultat_listen = kalkulator_objektet.fibonacci(antall)
+                
+            if antall < 1:
+                print("DUST\n"*100)
+            elif antall == 1:
+                print("0")
+            elif antall > 10000:
+                print("Maks nummer er 10 000")
+            else:
+                print(f"\nHele rekken: \n{resultat_listen[0]}")
+                print(f"\nDet {antall}. fibonaccitallet \n{resultat_listen[1]}")
         case _:
             print("Feil valg")
 
@@ -73,7 +96,7 @@ Denne delen er skjæringssetningen og Newton-Raphson tilnærmingsmetode
 den finner ut om det krysser en linje y = 0 i dette tilfellet og må
 omskrives til en generell version
 """
-
+'''
 from math import exp
 
 def f(x):
@@ -107,6 +130,7 @@ print("Skærer linjen y = 0\nAlle ytterpunktene av intervallene er på hver side
 print("Til nærmingen for [-5,-1]", round(tilnarming(-4),6))
 print("Til nærmingen for [-1,1]", round(tilnarming(0),6))
 print("Til nærmingen for [1,2]", round(tilnarming(1.5),6))
+'''
 
 """
 Slutt skjæringsetningen
