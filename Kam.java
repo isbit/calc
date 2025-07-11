3f25209119016fedd67fe350b04c00eb9de66f59
isbit <2607597+isbit@users.noreply.github.com>
2025-07-11 10:37:06 +0200

    Vet ikke hva jeg legger til her
    Men

--- Kam.java
+++ Kam.java
@@ -2,7 +2,15 @@ class Kam {
     public static void main(String[] args)
     {
         Vu v = new Vu("Homo");
-        System.out.println(v.getLegning());
+        //System.out.println(v.getLegning());
+
+        double x = 2.0;
+        double y = 1.0;
+        System.out.println((y == 1.0) ? x : 1.0 / x);
+        System.out.println(1.0 / x);
+
+
+        
     }
 }
 
---
+++ kam.py
@@ -0,0 +1,8 @@
+navn = "Kam"
+x = str("Kam")
+print(navn)
+print(x)
+print(type(navn))
+print(type(x))
+
+help(tuple)
